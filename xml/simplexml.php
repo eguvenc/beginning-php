2ab057b52f6ced357bd5853f8ad466f900f6aee7
fatihbulut <fatihbulut1991@gmail.com>
2013-08-28 20:14:32 +0300

    simplexml

---
+++ xml/simplexml.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$xml=simplexml_load_file("note.xml");
+
+echo $xml->to . "<br>";
+echo $xml->from . "<br>";
+echo $xml->heading . "<br>";
+echo $xml->body;
+?>  
+
+</body>
+</html>
